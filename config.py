3f5fa0f7e1bf055bf0c9cb1ef8f95df87666f288
Yijian ZHOU <34815814+YijianZhou@users.noreply.github.com>
2019-05-05 23:14:57 +0800

    Add files via upload

--- config.py
+++ config.py
@@ -1,10 +1,22 @@
 class Config(object):
   def __init__(self):
+    # Template selection params
+    self.max_dist = 100      # max epicentral distance for sta obs
+    self.win_len  = 45       # cut window length
+    self.t_blank  = 15       # time before P in the cut window
+    self.min_sta  = 4        # min sta num for a template events
+    self.p_thres  = 20       # min P STA/LTA (=trig_thres in PAL)
+    self.s_thres  = 10       # min S STA/LTA
+    self.fd_thres = 3.       # min value of dominant frequency
+    self.dt_p     = 1.5      # P arrival misfit
+    self.dt_s     = 3.       # S arrival misfit
+    # MFT params
     self.dt_trig = [1., 9.]
     self.dt_p = [1.,2.]
     self.dt_s = [0, 3]
-    self.trig_thres = 0.35
+    self.trig_thres = 0.3
     self.mask_len = 1.
+
     # data process
     self.decim_rate = 2
     self.freq_band = [1., 40.]
