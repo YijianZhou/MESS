0765bbe0af7d5d216a882d7b9603cbdde1395a3b
Yijian ZHOU <34815814+YijianZhou@users.noreply.github.com>
2019-06-28 21:00:12 +0800

    Add files via upload

--- mft_lib.py
+++ mft_lib.py
@@ -25,33 +25,21 @@ def calc_cc(data, temp):
     return cc
 
 
-def calc_masked_cc(temp_event, temp_dict, data_dict, 
-                   win_trig, trig_thres, mask_len, 
-                   date, samp_rate):
+def calc_masked_cc(temp_picks, data_dict, trig_thres, mask_len, samp_rate):
 
     t0=time.time()
     cc = []
-    temp_head = temp_event[0]
-    temp_picks = temp_event[1]
-    temp_name = temp_head[0]
-    temp_ot = temp_head[1]
 
-    for sta, [temp_tp, temp_ts] in temp_picks.items():
+    for sta, [temp_trig, _, _, _, _, _, dt_ot] in temp_picks.items():
 
         # get data
         if sta not in data_dict: continue
-        if sta not in temp_dict[temp_name]: continue
-        st = data_dict[sta]
-        temp = temp_dict[temp_name][sta]
-        temp_trig = temp.slice(temp_tp - win_trig[0], temp_tp + win_trig[1])
-
+        [stream, date, dt_st] = data_dict[sta]
+        
         # calc shifted cc trace
-        dt_st = st[0].stats.starttime - date
-        dt_ot = temp_ot - temp_tp + win_trig[0]
-        cci = calc_shifted_cc(temp_trig, st, dt_st + dt_ot, samp_rate)
-
+        cci = calc_shifted_cc(temp_trig, stream, dt_st + dt_ot, samp_rate)
         # calc masked cc trace & ppk
-        cci, num = mask_cc(cci, trig_thres, mask_len, date, samp_rate)
+        cci, num = mask_cc(cci, trig_thres, mask_len)
         cc.append(cci)
         print('{} process {} trigs | time {:.2f}'.format(sta, num, time.time()-t0))
 
@@ -59,12 +47,12 @@ def calc_masked_cc(temp_event, temp_dict, data_dict,
 
 
 # 1. calc cc trace & time shift
-def calc_shifted_cc(temp_trig, st, dt, samp_rate):
+def calc_shifted_cc(temp_trig, stream, dt, samp_rate):
 
     # calc trig cc
-    cci = calc_cc(st[0].data, temp_trig[0].data)
-    cci+= calc_cc(st[1].data, temp_trig[1].data)
-    cci+= calc_cc(st[2].data, temp_trig[2].data)
+    cci = calc_cc(stream[0].data, temp_trig[0].data)
+    cci+= calc_cc(stream[1].data, temp_trig[1].data)
+    cci+= calc_cc(stream[2].data, temp_trig[2].data)
     cci = cci / 3.
 
     # time shift to ot
@@ -76,7 +64,7 @@ def calc_shifted_cc(temp_trig, st, dt, samp_rate):
 
 
 # 2. mask cc trace
-def mask_cc(cci, trig_thres, mask_len, date, samp_rate):
+def mask_cc(cci, trig_thres, mask_len):
 
   # cc mask
   trig_idxs = np.where(cci > trig_thres)[0]
@@ -89,7 +77,6 @@ def mask_cc(cci, trig_thres, mask_len, date, samp_rate):
     trig_idx = trig_idxs[trig_idxs >= slide_idx][0]
     cc_max = np.amax(cci[trig_idx : trig_idx + mask_len])
     idx_max = np.argmax(cci[trig_idx : trig_idx + mask_len]) + trig_idx
-    oti = date + idx_max / samp_rate
     mask_idx0 = max(0, idx_max - mask_len //2)
     mask_idx1 = idx_max + mask_len//2
     cci[mask_idx0 : mask_idx1] = cc_max
@@ -116,7 +103,7 @@ def det_cc_stack(cc_stack, trig_thres, mask_len, date, samp_rate):
     cc_max  = np.amax(cc_stack[det_idx : det_idx + 2*mask_len])
     idx_max = np.where(cc_stack[det_idx: det_idx + 2*mask_len] == cc_max)
     idx_max = int(np.median(idx_max))
-    det_oti = date + (det_idx + idx_max) /samp_rate
+    det_oti = date + (det_idx + idx_max) / samp_rate
     det_ots.append([det_oti, cc_max])
     print('detection: ', det_oti, round(cc_max,2))
 
@@ -127,43 +114,36 @@ def det_cc_stack(cc_stack, trig_thres, mask_len, date, samp_rate):
 
 
 # 4. pick P&S by cc
-def ppk_cc(det_oti, temp_event, temp_dict, data_dict, 
+def ppk_cc(det_oti, temp_picks, data_dict, 
            win_p, win_s, picker, mask_len, samp_rate):
 
   picksi = []
-  temp_name = temp_event[0][0]
-  temp_ot = temp_event[0][1]
-  temp_picks = temp_event[1]
-  for sta, [temp_tp, temp_ts] in temp_picks.items():
+  for sta, [_, temp_p, temp_s, tp, ts, ot, _] in temp_picks.items():
 
     # get data
     if sta not in data_dict: continue
-    if sta not in temp_dict[temp_name]: continue
-    st = data_dict[sta]
-    temp = temp_dict[temp_name][sta]
-    temp_p = temp.slice(temp_tp - win_p[0], temp_tp + win_p[1])
-    temp_s = temp.slice(temp_ts - win_s[0], temp_ts + win_s[1])
+    [stream, _, dt_st] = data_dict[sta]
 
-    tpi0 = det_oti + (temp_tp - temp_ot)
-    tsi0 = det_oti + (temp_ts - temp_ot)
+    tpi0 = det_oti + (tp - ot)
+    tsi0 = det_oti + (ts - ot)
 
     # cut p&s data
-    p_rng = [win_p[0] +  mask_len /samp_rate,
-             win_p[1] +  mask_len /samp_rate]
-    s_rng = [win_s[0] +2*mask_len /samp_rate,
-             win_s[1] +2*mask_len /samp_rate]
-    st_p = st.slice(tpi0 - p_rng[0], tpi0 + p_rng[1])
-    st_s = st.slice(tsi0 - s_rng[0], tsi0 + s_rng[1])
+    p_rng = [win_p[0] +   mask_len / samp_rate,
+             win_p[1] +   mask_len / samp_rate]
+    s_rng = [win_s[0] + 2*mask_len / samp_rate,
+             win_s[1] + 2*mask_len / samp_rate]
+    st_p = stream.slice(tpi0 - p_rng[0], tpi0 + p_rng[1])
+    st_s = stream.slice(tsi0 - s_rng[0], tsi0 + s_rng[1])
     if len(st_p)!=3 or len(st_s)!=3: break
 
     # ppk by cc
     cc_p  = calc_cc(st_p[2].data, temp_p[2].data)
     cc_s0 = calc_cc(st_s[0].data, temp_s[0].data)
     cc_s1 = calc_cc(st_s[1].data, temp_s[1].data)
-    tpi = tpi0 - p_rng[0] + win_p[0] + np.argmax(cc_p) /samp_rate
-    tsi = tsi0 - s_rng[0] + win_s[0] + (np.argmax(cc_s0) + np.argmax(cc_s1)) /samp_rate /2.
+    tpi = tpi0 - p_rng[0] + win_p[0] + np.argmax(cc_p) / samp_rate
+    tsi = tsi0 - s_rng[0] + win_s[0] + (np.argmax(cc_s0) + np.argmax(cc_s1)) / samp_rate / 2.
     cc_p_max = np.amax(cc_p)
-    cc_s_max = (np.amax(cc_s0) + np.amax(cc_s1)) /2
+    cc_s_max = (np.amax(cc_s0) + np.amax(cc_s1)) / 2.
 
     # get S amplitude
     ampx = picker.get_amp(st_s[0].data)
--- run_mft.py
+++ run_mft.py
@@ -1,11 +1,10 @@
 import os, sys, glob
-sys.path.append('/home/zhouyj/Xiaojiang/PpkAssocLoc')
+sys.path.append('/home/zhouyj/Xiaojiang/PpkDet')
 import argparse
 import time
 import numpy as np
 from obspy import read, UTCDateTime
-from scipy.signal import correlate
-# import functions from PpkAssocLoc
+# import functions from PpkDet
 import data_pipeline as dp
 import pickers
 # import MFT functions
@@ -13,9 +12,26 @@ from mft_lib import *
 import config
 import warnings
 warnings.filterwarnings("ignore")
+import matplotlib.pyplot as plt
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--data_dir', type=str,
+                        default='/data3/XJ_SAC/[Y-Z]*/*')
+    parser.add_argument('--time_range', type=str,
+                        default='20160901,20190201')
+    parser.add_argument('--temp_root', type=str,
+                        default='./output/zsy/Templates')
+    parser.add_argument('--temp_pha', type=str,
+                        default='./output/zsy/temp_zsy.pha')
+    parser.add_argument('--out_ctlg', type=str,
+                        default='./output/zsy/aug_zsy.ctlg')
+    parser.add_argument('--out_pha', type=str,
+                        default='./output/zsy/aug_zsy.pha')
+    args = parser.parse_args()
 
 
-def preprocess(st, decim_rate, freq_band):
+def preprocess(st):
 
     # time alignment
     start_time = max([tr.stats.starttime for tr in st])
@@ -33,156 +49,138 @@ def preprocess(st, decim_rate, freq_band):
         return st.filter(flt_type, freqmin=freqmin, freqmax=freqmax)
 
 
-# get list of template events
-def get_temp_events():
+# make temp dict: location + sta obs
+def get_temp_dict():
 
-    f=open(args.temp_pha); lines=f.readlines(); f.close()
-    temp_events = []
-    for line in lines:
-      info = line.split(',')
-      if len(info)==5:
-        name = info[0]
-        ot   = UTCDateTime(name)
+  f=open(args.temp_pha); lines=f.readlines(); f.close()
+  temp_dict = {}
+  for line in lines:
+    info = line.split(',')
+    if len(info)==5:
+        temp_name = info[0]
+        ot   = UTCDateTime(temp_name)
         lat  = float(info[1])
         lon  = float(info[2])
         dep  = float(info[3])
-        temp_head= [name, ot, lat, lon, dep]
-        temp_events.append([temp_head, {}])
-      else:
+        temp_loc = [ot, lat, lon, dep]
+        temp_dict[temp_name] = [temp_loc, {}]
+    else:
         sta = info[0]
         tp = UTCDateTime(info[1])
         ts = UTCDateTime(info[2])
-        temp_events[-1][1][sta] = [tp, ts]
-    return temp_events
-
-
-# read continuous raw data and preprocess
-def read_data(data_dict, decim_rate, freq_band):
-
-    print('read continuous data')
-    todel=[]
-    for sta in data_dict:
-        if len(data_dict[sta])!=3:
-            todel.append(sta); continue
-        st = read(data_dict[sta][0])
-        st+= read(data_dict[sta][1])
-        st+= read(data_dict[sta][2])
-        st = preprocess(st, decim_rate, freq_band)
-        if len(st)!=3:
-            todel.append(sta); continue
-        data_dict[sta] = st
-    for sta in todel: data_dict.pop(sta)
-    return data_dict
-
-
-# read template data and preprocess
-def read_temp(temp_events, temp_root, decim_rate, freq_band):
+        dt_ot = ot - tp + win_trig[0]
+        temp_dict[temp_name][1][sta] = [tp, ts, dt_ot]
 
   print('read template data')
-  temp_dict = {}
-  for temp_event in temp_events:
-    temp_name = temp_event[0][0]
-    temp_picks = temp_event[1]
-    temp_dict[temp_name] = {}
+  t=time.time()
+  for i,temp_name in enumerate(temp_dict):
+    if i%50==0: print(i,'th template', temp_name, int(time.time()-t),'s')
+    temp_lov  = temp_dict[temp_name][0]
+    [ot, lat, lon, dep] = temp_loc
+    temp_picks = temp_dict[temp_name][1]
     for sta in temp_picks:
-        temp_paths = os.path.join(temp_root, temp_name, '*.%s.*'%sta)
+        [tp, ts, dt_ot] = temp_dict[temp_name][1][sta]
+        temp_paths = os.path.join(args.temp_root, temp_name, '*.%s.*'%sta)
         temp_paths = sorted(glob.glob(temp_paths))
         temp = read(temp_paths[0])
         temp+= read(temp_paths[1])
         temp+= read(temp_paths[2])
-        temp = preprocess(temp, decim_rate, freq_band)
+        temp = preprocess(temp)
         if len(temp)!=3: continue
-        temp_dict[temp_name][sta] = temp
+        temp_trig = temp.slice(tp - win_trig[0], tp + win_trig[1])
+        temp_p    = temp.slice(tp - win_p[0], tp + win_p[1])
+        temp_s    = temp.slice(ts - win_s[0], ts + win_s[1])
+        temp_dict[temp_name][1][sta] = [temp_trig, temp_p, temp_s, tp, ts, ot, dt_ot]
   return temp_dict
 
 
+# read continuous raw data and preprocess
+def read_data(data_dict):
 
-def main(args):
+    print('read continuous data')
+    todel=[]
+    t=time.time()
+    for sta in data_dict:
+        print(sta, int(time.time()-t),'s')
+        if len(data_dict[sta])!=3:
+            todel.append(sta); continue
+        stream = read(data_dict[sta][0])
+        stream+= read(data_dict[sta][1])
+        stream+= read(data_dict[sta][2])
+        stream = preprocess(stream)
+        if len(stream)!=3:
+            todel.append(sta); continue
+        dt_st = stream[0].stats.starttime - date
+        data_dict[sta] = [stream, date, dt_st]
+    for sta in todel: data_dict.pop(sta)
+    return data_dict
 
-  # MFT params
-  cfg = config.Config()
-  decim_rate = cfg.decim_rate
-  freq_band = cfg.freq_band
-  samp_rate = 100./decim_rate
-  win_trig = cfg.win_trig
-  win_p = cfg.win_p
-  win_s = cfg.win_s
-  trig_thres = cfg.trig_thres
-  mask_len = int(samp_rate *cfg.mask_len)
-  picker = pickers.Trad_PS()
 
-  # i/o file
-  out_ctlg = open(args.out_ctlg,'w')
-  out_pha  = open(args.out_pha, 'w')
-  temp_events = get_temp_events()
-  temp_root = args.temp_root
-  temp_dict = read_temp(temp_events, temp_root, decim_rate, freq_band)
 
-  # get time range
-  start_date = UTCDateTime(args.time_range.split(',')[0])
-  end_date   = UTCDateTime(args.time_range.split(',')[1])
-  print('Run MFT')
-  print('time range: {} to {}'.format(start_date, end_date))
+# MFT params
+cfg = config.Config()
+decim_rate = cfg.decim_rate
+freq_band = cfg.freq_band
+samp_rate = 100./decim_rate
+win_trig = cfg.win_trig
+win_p = cfg.win_p
+win_s = cfg.win_s
+trig_thres = cfg.trig_thres
+mask_len = int(samp_rate *cfg.mask_len)
+picker = pickers.Trad_PS()
 
-  # for all days
-  num_day = (end_date.date - start_date.date).days
-  for day_idx in range(num_day):
+# i/o file
+out_ctlg = open(args.out_ctlg,'w')
+out_pha  = open(args.out_pha, 'w')
+temp_dict = get_temp_dict()
+
+# get time range
+start_date = UTCDateTime(args.time_range.split(',')[0])
+end_date   = UTCDateTime(args.time_range.split(',')[1])
+print('Run MFT (cpu version)')
+print('time range: {} to {}'.format(start_date, end_date))
+
+# for all days
+num_day = (end_date.date - start_date.date).days
+for day_idx in range(num_day):
 
     # get data paths
     date = start_date + day_idx*86400
     data_dict = dp.get_xj(args.data_dir, date)
     if data_dict=={}: continue
     # read data and preprocess
-    data_dict = read_data(data_dict, decim_rate, freq_band)
+    data_dict = read_data(data_dict)
     print('-'*40)
     print('detecting %s'%date.date)
 
     # run MFT with all templates
-    for temp_event in temp_events:
+    for temp_name in temp_dict:
 
         # init
         t0=time.time()
-        temp_head = temp_event[0]
-        temp_picks = temp_event[1]
+        temp_loc = temp_dict[temp_name][0]
+        temp_picks = temp_dict[temp_name][1]
         print('-'*40)
-        print('template ', temp_head)
+        print('template ', temp_loc)
         if len([sta for sta in temp_picks if sta in data_dict])<4: continue
 
         # for each station, calc masked cc trace
-        cc = calc_masked_cc(temp_event, temp_dict, data_dict, 
-                            win_trig, trig_thres, mask_len, date, samp_rate)
+        cc = calc_masked_cc(temp_picks, data_dict, trig_thres, mask_len, samp_rate)
 
         # detect with stacked cc trace
         if len(cc)<4: continue
         cc_stack = np.sum(cc,axis=0) / len(cc)
+#        plt.plot(cc_stack); plt.show()
         det_ots = det_cc_stack(cc_stack, trig_thres, mask_len, date, samp_rate)
         print('{} detections | time {:.2f}'.format(len(det_ots), time.time()-t0))
         if len(det_ots)==0: continue
 
         # ppk by cc
         for [det_oti, det_cci] in det_ots:
-            picksi = ppk_cc(det_oti, temp_event, temp_dict, data_dict, 
+            picksi = ppk_cc(det_oti, temp_picks, data_dict, 
                             win_p, win_s, picker, mask_len, samp_rate)
-            write_det_ppk(det_oti, det_cci, temp_head[2:], picksi, out_ctlg, out_pha)
+            write_det_ppk(det_oti, det_cci, temp_loc[1:], picksi, out_ctlg, out_pha)
         print('time consumption: {:.2f}'.format(time.time()-t0))
 
-  out_ctlg.close()
-  out_pha.close()
-
-
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--data_dir', type=str,
-                        default='/data3/XJ_SAC/*/*')
-    parser.add_argument('--time_range', type=str,
-                        default='20160901,20190201')
-    parser.add_argument('--temp_root', type=str,
-                        default='./output/ZSY/Templates')
-    parser.add_argument('--temp_pha', type=str,
-                        default='./output/ZSY/pha_ZSY_temp.dat') 
-    parser.add_argument('--out_ctlg', type=str,
-                        default='./output/ZSY/catalog_aug_ZSY.dat')
-    parser.add_argument('--out_pha', type=str,
-                        default='./output/ZSY/phase_aug_ZSY.dat')
-    args = parser.parse_args()
-    main(args)
+out_ctlg.close()
+out_pha.close()
