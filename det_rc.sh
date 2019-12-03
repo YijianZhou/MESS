c6b35b9e4dc1a7537adcb3db4e00e60e4113d3b2
Yijian ZHOU <34815814+YijianZhou@users.noreply.github.com>
2019-12-03 13:22:53 +0800

    Update det_rc.sh

--- det_rc.sh
+++ det_rc.sh
@@ -1,7 +1,7 @@
 t0='20190707'
 t1='20190710'
 net='rc'
-python run_mft_gpu.py --time_range=$t0,$t1 \
+python run_msms_gpu.py --time_range=$t0,$t1 \
 --data_dir=/data2/Ridgecrest/*/* \
 --out_pha=./output/$net/phase_${t0}_${t1}_aug.dat \
 --out_ctlg=./output/$net/catalog_${t0}_${t1}_aug.dat \
