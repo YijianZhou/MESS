457549ecac83e891b7238608df693d8cb23c8455
Yijian ZHOU <zhouyj.pku@gmail.com>
2020-01-26 18:09:32 +0800

    Update det_one.py

--- run_msms/det_one.py
+++ run_msms/det_one.py
@@ -7,8 +7,8 @@ date_rng = '20160901-20190201'
 out_root = './output/zsy'
 temp_root = os.path.join(out_root, 'Templates')
 temp_pha = os.path.join(out_root, 'zsy_201609-201901_hyp.pha')
-out_ctlg = os.path.join(out_root, 'phase_{}.dat'.format(date_rng))
-out_pha = os.path.join(out_root, 'catalog_{}.dat'.format(date_rng))
+out_ctlg = os.path.join(out_root, 'catalog_{}.dat'.format(date_rng))
+out_pha = os.path.join(out_root, 'phase_{}.dat'.format(date_rng))
 
 # run MSMS cut temp
 shutil.copyfile('config.py', os.path.join(msms_dir, 'config.py'))
