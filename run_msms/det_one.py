86ad18ae5fa3d6f74609dc1c97041b4125a5eaf9
Yijian ZHOU <zhouyj.pku@gmail.com>
2020-02-06 17:56:28 +0800

    Add files via upload

--- run_msms/det_one.py
+++ run_msms/det_one.py
@@ -1,23 +1,28 @@
 import os, shutil
+from obspy import UTCDateTime
 
 # i/o paths
 msms_dir = '/home/zhouyj/software/MSMS'
 data_dir = '/data2/ZSY_SAC/*/*'
 date_rng = '20160901-20190201'
-out_root = './output/zsy'
-temp_root = os.path.join(out_root, 'Templates')
-temp_pha = os.path.join(out_root, 'zsy_201609-201901_hyp.pha')
-out_ctlg = os.path.join(out_root, 'catalog_{}.dat'.format(date_rng))
-out_pha = os.path.join(out_root, 'phase_{}.dat'.format(date_rng))
+num_days = 60 # per file
+out_root = 'output/zsy'
+temp_root = 'input/zsy/Templates'
+temp_pha = 'input/zsy/zsy_201609-201901_hyp.pha'
 
 # run MSMS cut temp
 shutil.copyfile('config.py', os.path.join(msms_dir, 'config.py'))
-os.system("python {}/run_msms_gpu.py \
-    --data_dir={} \
-    --date_range={} \
-    --temp_root={} \
-    --temp_pha={} \
-    --out_ctlg={} \
-    --out_pha={}"\
-    .format(msms_dir, data_dir, date_rng, temp_root, temp_pha, out_ctlg, out_pha))
+start_date, end_date = [UTCDateTime(date) for date in date_rng.split('-')]
+num_files = int((end_date - start_date) / (num_days * 86400)) + 1
+for i in range(num_files):
+    t0 = ''.join(str((start_date + i*num_days*86400).date).split('-'))
+    t1 = ''.join(str((start_date + (i+1)*num_days*86400).date).split('-'))
+    date_rng = '{}-{}'.format(t0, t1)
+    out_pha = '{}/phase_{}.dat'.format(out_root, date_rng)
+    out_ctlg = '{}/catalog_{}.dat'.format(out_root, date_rng)
+    os.system("python {}/run_msms_gpu.py \
+        --data_dir={} --date_range={} \
+        --temp_root={} --temp_pha={} \
+        --out_ctlg={} --out_pha={}"\
+        .format(msms_dir, data_dir, date_rng, temp_root, temp_pha, out_ctlg, out_pha))
 
