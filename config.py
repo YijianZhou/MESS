9c8d3e76fe2b2b912773f40d37c16eb8103cb410
Yijian ZHOU <34815814+YijianZhou@users.noreply.github.com>
2020-12-16 16:01:07 +0800

    Update config.py

--- config.py
+++ config.py
@@ -10,7 +10,7 @@ class Config(object):
     self.win_len = [15,25]  # cut window length
     self.min_sta = 4        # min sta num for a template events
     self.max_sta = 15
-    self.get_data_dict = dp.get_rc_data
+    self.get_data_dict = dp.get_data_dict
     self.get_sta_dict = dp.get_sta_dict
 
     # MFT params
@@ -24,7 +24,7 @@ class Config(object):
     self.pick_win_s = [2.5, 2.5]   # win for S pick
     self.chn_p = [2]
     self.chn_s = [0,1]
-    self.amp_win = [1, 5]
+    self.amp_win = [1, 5]          # win for get_amp, sec pre&post P
 
     # data process
     self.samp_rate = 50
