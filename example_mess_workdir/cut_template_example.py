2d09f304363f053624496dff3a9a36db7d894a3f
Yijian ZHOU <34815814+YijianZhou@users.noreply.github.com>
2020-12-18 11:13:32 +0800

    little change

--- example_mess_workdir/cut_template_example.py
+++ example_mess_workdir/cut_template_example.py
@@ -5,11 +5,11 @@ import os, shutil
 # i/o paths
 mess_dir = '/home/zhouyj/software/MESS'
 data_dir = '/data2/Ridgecrest'
-out_root = '/data4/bigdata/zhouyj/RC_templates'
-temp_pha = 'input/rc_pad.temp'
+out_root = '/data4/bigdata/zhouyj/Example_templates'
+temp_pha = 'input/example_pad.temp'
 
 # cut template data
-shutil.copyfile('config_rc.py', os.path.join(mess_dir, 'config.py'))
+shutil.copyfile('config_example.py', os.path.join(mess_dir, 'config.py'))
 os.system("python {}/cut_template_sac.py \
     --data_dir={} --temp_pha={} --out_root={}"\
     .format(mess_dir, data_dir, temp_pha, out_root))
--- example_mess_workdir/run_mess_gpu_example.py
+++ example_mess_workdir/run_mess_gpu_example.py
@@ -3,17 +3,17 @@ import os, shutil
 # i/o paths
 gpu_idx = '1'
 mess_dir = '/home/zhouyj/software/MESS'
-data_dir = '/data2/Ridgecrest'
+data_dir = '/data2/Example_data'
 time_range = '20190705-20190710'
-sta_file = 'input/rc_scsn_pad.sta'
-temp_root = '/data3/bigdata/zhouyj/RC_templates'
-temp_pha = 'input/rc_pad.temp'
-out_root = 'output/rc'
+sta_file = 'input/example_pad.sta'
+temp_root = '/data3/bigdata/zhouyj/Example_templates'
+temp_pha = 'input/example_pad.temp'
+out_root = 'output/example'
 out_ctlg = os.path.join(out_root, 'catalog_{}.dat'.format(time_range))
 out_pha = os.path.join(out_root, 'phase_{}.dat'.format(time_range))
 
 # run MESS (gpu ver)
-shutil.copyfile('config_rc.py', os.path.join(mess_dir, 'config.py'))
+shutil.copyfile('config_example.py', os.path.join(mess_dir, 'config.py'))
 os.system("python {}/run_mess_gpu.py --gpu_idx={} \
     --data_dir={}  --time_range={} --sta_file={} \
     --temp_root={} --temp_pha={} \
