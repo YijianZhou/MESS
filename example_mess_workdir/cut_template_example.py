d0fa822f89efd898a328d2371d0b10fe6ebc7756
Yijian ZHOU <34815814+YijianZhou@users.noreply.github.com>
2021-05-26 21:24:20 +0800

    Update cut_template_example.py

--- example_mess_workdir/cut_template_example.py
+++ example_mess_workdir/cut_template_example.py
@@ -8,8 +8,7 @@ data_dir = '/data/Example_data'
 out_root = 'output/Example_templates'
 temp_pha = 'input/example.temp'
 
-# run MSMS cut temp
-shutil.copyfile('config_rc.py', os.path.join(mess_dir, 'config.py'))
+shutil.copyfile('config_example.py', os.path.join(mess_dir, 'config.py'))
 os.system("python {}/cut_template_torch.py \
     --data_dir={} --temp_pha={} --out_root={}"\
     .format(mess_dir, data_dir, temp_pha, out_root))
