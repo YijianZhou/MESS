9fe9226a66979795f450847526709d07030f604e
Yijian ZHOU <34815814+YijianZhou@users.noreply.github.com>
2022-06-26 00:26:01 -0700

    update prep

--- dataset.py
+++ dataset.py
@@ -186,12 +186,10 @@ def preprocess(stream):
     st = stream.slice(start_time, end_time)
     # resample data
     org_rate = int(st[0].stats.sampling_rate)
-    rate = np.gcd(org_rate, samp_rate)
-    if rate==1: print('warning: bad sampling rate!'); return []
-    decim_factor = int(org_rate / rate)
-    resamp_factor = int(samp_rate / rate)
-    st = st.decimate(decim_factor)
-    if resamp_factor!=1: st = st.interpolate(samp_rate)
+    if org_rate!=samp_rate: st = st.interpolate(samp_rate)
+    for ii in range(3):
+        st[ii].data[np.isnan(st[ii].data)] = 0
+        st[ii].data[np.isinf(st[ii].data)] = 0
     # filter
     st = st.detrend('demean').detrend('linear').taper(max_percentage=0.05, max_length=10.)
     freq_min, freq_max = freq_band
--- dataset_gpu.py
+++ dataset_gpu.py
@@ -187,6 +187,9 @@ def preprocess(stream):
     # resample data
     org_rate = int(st[0].stats.sampling_rate)
     if org_rate!=samp_rate: st = st.interpolate(samp_rate)
+    for ii in range(3):
+        st[ii].data[np.isnan(st[ii].data)] = 0
+        st[ii].data[np.isinf(st[ii].data)] = 0
     # filter
     st = st.detrend('demean').detrend('linear').taper(max_percentage=0.05, max_length=10.)
     freq_min, freq_max = freq_band
