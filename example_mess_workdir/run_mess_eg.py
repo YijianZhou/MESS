735865562d406b4cce669f3ec1ddd9c533389e8c
Yijian ZHOU <34815814+YijianZhou@users.noreply.github.com>
2022-10-23 13:50:16 -0700

    run MESS by time seg

--- example_mess_workdir/run_mess_eg.py
+++ example_mess_workdir/run_mess_eg.py
@@ -1,4 +1,5 @@
 import os, shutil
+from obspy import UTCDateTime
 
 # i/o paths
 mess_dir = '/home/zhouyj/software/MESS'
@@ -8,12 +9,20 @@ sta_file = 'input/example_pal_format1.sta'
 temp_root = 'output/Example_templates'
 temp_pha = 'input/eg_pal.temp'
 out_root = 'output/eg'
-out_ctlg = os.path.join(out_root, 'catalog_{}.dat'.format(time_range))
-out_pha = os.path.join(out_root, 'phase_{}.dat'.format(time_range))
+# divide into time segments
+tmin, tmax = [UTCDateTime(ti) for ti in time_range.split('-')]
+num_days = 60
+num_segs = ((tmax.date - tmin.date).days) // num_days + 1
 
 # run MESS (cpu ver)
 shutil.copyfile('config_eg.py', os.path.join(mess_dir, 'config.py'))
-os.system("python {}/run_mess.py \
+for ii in range(num_segs):
+  t0, t1 = tmin+ii*num_days*86400, tmin+(ii+1)*num_days*86400
+  if t1>tmax: t1 = tmax
+  time_range = '-'.join([''.join(str(ti.date).split('-')) for ti in [t0,t1]])
+  out_ctlg = os.path.join(out_root, 'catalog_{}.dat'.format(time_range))
+  out_pha = os.path.join(out_root, 'phase_{}.dat'.format(time_range))
+  os.system("python {}/run_mess.py \
     --data_dir={}  --time_range={} --sta_file={} \
     --temp_root={} --temp_pha={} \
     --out_ctlg={} --out_pha={}"\
